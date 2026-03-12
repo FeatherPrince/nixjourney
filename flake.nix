a0e0ecfdf065f87427983676ef046391203c87c0
feather <featherprinceyt@gmail.com>
2026-03-12 02:49:59 +0000

    added basic nix files

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager/nixpkgs-unstable";
+			inputs.nixpkgs.follows = "nixpkgs";
+		}
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+		nixosConfigurations.nixos-host = nixpkgs.lib.nixeosSystem {
+			system = "x86_64-linux";
+			modules = [
+				./configuration.nix
+				home-manager.nixosModules.home.manager
+				{
+					home-manager = {
+						useGlobalPkgs = true;
+						useUserPackages = true;
+						users.feather = import ./home.nix;
+						backupFileExtension = "backup";
+					}
+				}
+			]
+		}
+  };
+}
