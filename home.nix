07238331b4742313bb78d0929a99a8a33465f0ed
feather <featherprinceyt@gmail.com>
2026-03-12 20:18:08 +0000

    fixed WSL hardware confix bug

--- home.nix
+++ home.nix
@@ -13,13 +13,13 @@
 			ls = "eza --icons=always -X -F=always";
 			nrs = "sudo nixos-rebuild switch";
 		};
-	
+	# there are issues with multi line prompts in WSL
 		initExtra = ''
-	#initExtra commands executed in interactive shells
-	#export PS1='┌[\u@\H][\t]\n└[\w][\$]'
-	#export PS1='\[\e[91;2m\]┌[\[\e[0m\]\u@\H\[\e[91;2m\]][\[\e[0m\]\t\[\e[91;2m\]]\n└[\[\e[0m\]\w\[\e[91;2m\]][\[\e[0m\]\$\[\e[91;2m\]]\[\e[0m\]'
+	# initExtra commands executed in interactive shells
+	# export PS1='┌[\u@\H][\t]\n└[\w][\$]'
+	# export PS1='\[\e[91;2m\]┌[\[\e[0m\]\u@\H\[\e[91;2m\]][\[\e[0m\]\t\[\e[91;2m\]]\n└[\[\e[0m\]\w\[\e[91;2m\]][\[\e[0m\]\$\[\e[91;2m\]]\[\e[0m\]'
 	export PS1='\[\e[91m\]┌\[\e[91m\][\[\e[0m\]\u@\H\[\e[91m\]]\[\e[91m\][\[\e[0m\]\t\[\e[91m\]]\n\[\e[91m\]└\[\e[91m\][\[\e[0m\]\w\[\e[91m\]]\[\e[91m\][\[\e[0m\]\$\[\e[91m\]]\[\e[0m\]'
-	#exec fish
+	# exec fish
 	'';
 	};
 }
