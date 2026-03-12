10676d3ad1a3daa9953ea8aedd6051893eb5ead6
feather <featherprinceyt@gmail.com>
2026-03-12 03:26:41 +0000

    typo

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
 	inputs = {
 	nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager/nixpkgs-unstable";
+		home-manager-unstable = {
+			url = "github:nix-community/home-manager";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
   };
@@ -14,8 +14,7 @@
 			system = "x86_64-linux";
 			modules = [
 				./configuration.nix
-				home-manager.nixosModules.home.manager
-				{
+				home-manager.nixosModules.home.manager {
 					home-manager = {
 						useGlobalPkgs = true;
 						useUserPackages = true;
