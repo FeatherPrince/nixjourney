bb3e27960b67a84b79e196e255e80a52bf03e10f
feather <featherprinceyt@gmail.com>
2026-03-13 21:21:16 +0100

    comment out package reference command in update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,2 +1,2 @@
-sudo nix-store -q --references /var/run/current-system/sw | cut -d'-' -f2- > currentlyInstalledPackages
+# sudo nix-store -q --references /var/run/current-system/sw | cut -d'-' -f2- > currentlyInstalledPackages
 sudo nixos-rebuild switch --flake --impure
