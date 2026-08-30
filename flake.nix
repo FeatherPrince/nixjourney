e06952c31316334f0c161cea4b54cf3afee3a43c
Feather <featherprinceyt@gmail.com>
2026-08-30 15:38:14 +0200

    added flake configurations

--- flake.nix
+++ flake.nix
@@ -10,29 +10,36 @@
 	};
 
 	outputs = { self, nixpkgs, home-manager, ... }:
+# ```let in``` lets user define variables inside of the flake, while specialArgs allow other files to use it
 	let
 		userName = "feather";
+		stateVersion = "25.11";
+		hostName = "nix-host";
 	in
 	{
-		nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-			system = "x86_64-linux";
-	specialArgs = {
-		stateVersion = "25.11";
-		userName = "feather";
-		hostName = "nix-host";
-	};
-			modules = [
-				./configuration.nix
-				home-manager.nixosModules.home-manager {
-					home-manager = {
-						useGlobalPkgs = true;
-						useUserPackages = true;
-						backupFileExtension = "backup";
-						extraSpecialArgs = { inherit userName; };
-						users.${userName} = import ./home.nix;
-					};
-				}
-			];
+	  nixosConfigurations = {
+     	nixos = nixpkgs.lib.nixosSystem {
+     	};
+  		hypr = nixpkgs.lib.nixosSystem {
+  			system = "x86_64-linux";
+  	specialArgs = {
+  		stateVersion = stateVersion;
+  		userName = userName;
+  		hostName = hostName;
+  	};
+  			modules = [
+  				./configuration.nix
+  				home-manager.nixosModules.home-manager {
+  					home-manager = {
+  						useGlobalPkgs = true;
+  						useUserPackages = true;
+  						backupFileExtension = "backup";
+  						extraSpecialArgs = { inherit userName; };
+  						users.${userName} = import ./home.nix;
+  					};
+  				}
+  			];
+  		};
 		};
 	};
 }
