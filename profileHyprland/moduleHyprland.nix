4138d44959eefe3b5ad315dccd6b431d0add170d
Feather <featherprinceyt@gmail.com>
2026-08-30 15:37:17 +0200

    changed naming convention and fixed file paths

--- profileHyprland/moduleHyprland.nix
+++ profileHyprland/moduleHyprland.nix
@@ -40,10 +40,12 @@
 
 
 		# This creates a symlink at ~/.config/hypr/hyprland.lua
-		xdg.configFile."hypr/hyprland.lua".source = ./configs/hyprland.lua;
+		xdg.configFile."hypr/hyprland.lua".source = ../configs/hyprland.lua;
 		# xdg.configFile."hypr/hyprlauncher.conf".source = ./configs/hyprlauncher.conf;
 
 		# You can even do it for the whole folder
 		#	xdg.configFile."waybar".source = ./configs/waybar-folder;
+		services.wayle.enable = true;
+	  services.wayle.autoInstallDependencies = true;
 	};
 }
