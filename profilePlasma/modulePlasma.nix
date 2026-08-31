d34c711ff3966e69b37ce4af3934ca0834a53edd
Feather <featherprinceyt@gmail.com>
2026-09-01 00:14:13 +0200

    initial plasma config

---
+++ profilePlasma/modulePlasma.nix
@@ -0,0 +1,13 @@
+{ pkgs,  ... }:
+
+{
+	# environment.systemPackages = with pkgs; [
+	# 	pkgs.kdePackages.plasma-login-manager
+	# ];
+	services.desktopManager.plasma6.enable = true;
+	services.displayManager.plasma-login-manager.enable = true;
+	services.desktopManager.plasma6.enableQt5Integration = true;
+	programs.partition-manager.enable = true;
+	programs.k3b.enable = true;
+	xdg.portal.extraPortals = [ xdg-desktop-portal-kde ];
+}
