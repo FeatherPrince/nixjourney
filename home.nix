fdfef36701d02bf516907014c5764b5316be2cc5
Feather <featherprinceyt@gmail.com>
2026-09-01 18:38:23 +0200

    yt-dlp config + default download directory is ~/Videos/yt-dlp

--- home.nix
+++ home.nix
@@ -8,20 +8,34 @@
   # programs.oh-my-posh.enableZshIntegration = true;
   # programs.oh-my-posh.useTheme = "";
 
-  programs.bash = {
+	  programs.bash = {
 		enable = true;
   	# bashrcExtra commands executed in non all shells including non interactive ones
 	# 	bashrcExtra = ''
 	# '';
 		shellAliases = {
 			ls = "eza --icons=always -X -F=always";
-      cat = "bat";
+	cat = "bat";
 			# nrs = "sudo nixos-rebuild switch";
 		};
 	  # initExtra commands executed in interactive shells
 	# 	initExtra = ''
 	# '';
 	};
-	services.clipse.enable = true;
-	services.hyprpolkitagent.enable = true;
+	# services.clipse.enable = true;
+	# services.hyprpolkitagent.enable = true;
+
+	programs.yt-dlp.enable = true;
+	programs.yt-dlp.extraConfig = "-P ~/Videos/yt-dlp";
+	programs.yt-dlp.settings = {
+		# color = [
+		# 	"stdout:no_color"
+		#     "stderr:always"
+		# ];
+		# downloader = "aria2c";
+		# downloader-args = "aria2c:'-c -x8 -s8 -k1M -P ~/Videos/yt-dlp'";
+		# embed-subs = true;
+		# embed-thumbnail = true;
+		# sub-langs = "all";
+	};
 }
