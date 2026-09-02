d4a22a3cd7c11dfe3e6e7bb54389ee9b30abed17
Feather <featherprinceyt@gmail.com>
2026-09-03 01:07:25 +0200

    Update locale.nix

--- locale.nix
+++ locale.nix
@@ -16,17 +16,17 @@
 	];
 	i18n.imperativeLocale = true;
 
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "pl_PL.UTF-8";
-		LC_IDENTIFICATION = "pl_PL.UTF-8";
-		LC_MEASUREMENT = "pl_PL.UTF-8";
-		LC_MONETARY = "pl_PL.UTF-8";
-		LC_NAME = "pl_PL.UTF-8";
-		LC_NUMERIC = "pl_PL.UTF-8";
-		LC_PAPER = "pl_PL.UTF-8";
-		LC_TELEPHONE = "pl_PL.UTF-8";
-		LC_TIME = "pl_PL.UTF-8";
-	};
+	# i18n.extraLocaleSettings = {
+	# 	LC_ADDRESS = "pl_PL.UTF-8";
+	# 	LC_IDENTIFICATION = "pl_PL.UTF-8";
+	# 	LC_MEASUREMENT = "pl_PL.UTF-8";
+	# 	LC_MONETARY = "pl_PL.UTF-8";
+	# 	LC_NAME = "pl_PL.UTF-8";
+	# 	LC_NUMERIC = "pl_PL.UTF-8";
+	# 	LC_PAPER = "pl_PL.UTF-8";
+	# 	LC_TELEPHONE = "pl_PL.UTF-8";
+	# 	LC_TIME = "pl_PL.UTF-8";
+	# };
 	console.keyMap = "pl2";
 
 	environment = {
