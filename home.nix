812d041197c0078459a8bcfc8a6d6ba6a4b15910
Feather <featherprinceyt@gmail.com>
2026-09-04 15:44:06 +0200

    added a yt-dlp alias that makes downloads appear in XDG VIDEOS

--- home.nix
+++ home.nix
@@ -1,41 +1,29 @@
 { userName, ... }:
 
 {
-	home.username = userName;
+	home.username = "${userName}";
 	home.homeDirectory = "/home/${userName}";
 
   # programs.oh-my-posh.enable = true;
   # programs.oh-my-posh.enableZshIntegration = true;
   # programs.oh-my-posh.useTheme = "";
 
-	  programs.bash = {
+	programs.bash = {
 		enable = true;
-  	# bashrcExtra commands executed in non all shells including non interactive ones
+	# bashrcExtra commands executed in non all shells including non interactive ones
 	# 	bashrcExtra = ''
 	# '';
 		shellAliases = {
 			ls = "eza --icons=always -X -F=always";
-	cat = "bat";
+			cat = "bat";
+			yt-dlp = "yt-dlp -P $(xdg-user-dir VIDEOS)/yt-dlp";
 			# nrs = "sudo nixos-rebuild switch";
 		};
 	  # initExtra commands executed in interactive shells
 	# 	initExtra = ''
 	# '';
 	};
-	# services.clipse.enable = true;
-	# services.hyprpolkitagent.enable = true;
 
 	programs.yt-dlp.enable = true;
-	programs.yt-dlp.extraConfig = "-P ~/Videos/yt-dlp";
-	programs.yt-dlp.settings = {
-		# color = [
-		# 	"stdout:no_color"
-		#     "stderr:always"
-		# ];
-		# downloader = "aria2c";
-		# downloader-args = "aria2c:'-c -x8 -s8 -k1M -P ~/Videos/yt-dlp'";
-		# embed-subs = true;
-		# embed-thumbnail = true;
-		# sub-langs = "all";
-	};
+	# programs.yt-dlp.extraConfig = ''-P $(xdg-user-dir VIDEOS)/yt-dlp''; # this works, just not when used in the config
 }
